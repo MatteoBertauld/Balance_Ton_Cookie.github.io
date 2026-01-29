e1130abf5c27b3b06a0055ef8d9ea2c55979242c
matteo <matteobertauld@gmail.com>
2026-01-29 10:46:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// JS - La Logique
+let count = 0;
+const counterDisplay = document.getElementById('counter');
+const cookieBtn = document.getElementById('cookie-btn');
+
+cookieBtn.addEventListener('click', () => {
+count++;
+counterDisplay.innerText = count;
+
+// Petit bonus : effet de vibration aléatoire au clic
+cookieBtn.style.transform = `scale(0.95) rotate(${Math.random() * 10 - 5}deg)`;
+setTimeout(() => {
+        cookieBtn.style.transform = 'scale(1) rotate(0deg)';
+    }, 50);
+});
